19f3e421a31b877576ad8acb055c346798c6d46d
Eddie Stump <ejstump13@gmail.com>
2024-06-17 17:40:13 -0400

    Commented and Edited ControlPanel class

--- src/ControlPanel.java
+++ src/ControlPanel.java
@@ -8,24 +8,18 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class ControlPanel extends JPanel
-{
-    private DrawingPanel canvas;
-    private JButton updateButton;
-    private JComboBox<String> choosePlanetType, moveView;
+public class ControlPanel extends JPanel {
+    private final DrawingPanel canvas;
+    private final JComboBox<String> choosePlanetType;
+    private final JComboBox<String> moveView;
     private boolean isRunning = false;
     private Timer timer;
-    private TimerTask task;
 
-    public ControlPanel(DrawingPanel canvas)
-    {
+    public ControlPanel(DrawingPanel canvas) {
         this.canvas = canvas;
 
-
-
-        updateButton = new JButton("Toggle Motion");
+        JButton updateButton = new JButton("Toggle Motion");
         updateButton.addActionListener(new ColorButtonListener());
-
 
         String[] planetTypeNames = {"Sun", "Planet", "Satellite Close", "Satellite Far", "Planet Far"};
         choosePlanetType = new JComboBox<>(planetTypeNames);
@@ -37,8 +31,6 @@ public class ControlPanel extends JPanel
 
         moveView.addActionListener(new viewButtonListener());
 
-
-
         add(updateButton);
 
         add(moveView);
@@ -46,55 +38,41 @@ public class ControlPanel extends JPanel
         add(choosePlanetType);
     }
 
-    private class ColorButtonListener
-            implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
-            if(!isRunning){
+    private class ColorButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (!isRunning) {
 
                 isRunning = true;
 
                 timer = new Timer();
-                task = new Updater(canvas);
+                TimerTask task = new Updater(canvas);
 
                 timer.schedule(task, 500, 10);
 
-            }else {
+            } else {
                 timer.cancel();
                 isRunning = false;
             }
-            //canvas.updatePlanets();
-            //canvas.requestFocus();
-
-
-
         }
     }
 
-    private class PlanetButtonListener
-            implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
+    private class PlanetButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             canvas.addPlanet(choosePlanetType.getSelectedIndex() + 1);
             canvas.requestFocus();
         }
     }
 
-    private class viewButtonListener
-            implements ActionListener
-    {
-        public void actionPerformed(ActionEvent e)
-        {
+    private class viewButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             canvas.moveView(moveView.getSelectedIndex() + 1);
             canvas.requestFocus();
         }
     }
 }
 
-class Updater extends TimerTask{
-    private DrawingPanel canvas;
+class Updater extends TimerTask {
+    private final DrawingPanel canvas;
     public Updater(DrawingPanel c){
         canvas = c;
 
