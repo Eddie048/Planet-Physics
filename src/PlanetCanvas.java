a05f413568aa076de462d33ec86789a7244d0568
Eddie Stump <ejstump13@gmail.com>
2024-06-21 02:12:13 -0400

    Fixed several issues with new view

--- src/PlanetCanvas.java
+++ src/PlanetCanvas.java
@@ -18,8 +18,8 @@ public class PlanetCanvas extends JPanel {
 
     private boolean isNormalMode;
 
-    private final double fovBottom;
-    private final double fovTop;
+    private double fovBottom;
+    private double fovTop;
 
     public PlanetCanvas() {
 
@@ -35,8 +35,9 @@ public class PlanetCanvas extends JPanel {
         shiftY = 0;
         zoom = 1;
 
-        fovBottom = Math.atan(-0.5);
-        fovTop = Math.atan(0.5);
+        // Bottom and top of viewpoints, measured in radians, in the range -pi to pi
+        fovBottom = -0.25 * Math.PI;
+        fovTop = 0.25 * Math.PI;
 
         // Initialize follow planet to -1
         followPlanet = -1;
@@ -49,12 +50,50 @@ public class PlanetCanvas extends JPanel {
             public void keyPressed(KeyEvent e) {
                 int key = e.getKeyCode();
 
-                if (key == KeyEvent.VK_W || key == KeyEvent.VK_UP) shiftY += 100;
-                else if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) shiftX += 100;
-                else if (key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN) shiftY -= 100;
-                else if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) shiftX -= 100;
-                else if (key == KeyEvent.VK_1) zoom *= 2;
-                else if (key == KeyEvent.VK_2) zoom /= 2;
+                switch (key) {
+                    case KeyEvent.VK_W:
+                    case KeyEvent.VK_UP:
+                        if (isNormalMode) shiftY += 100;
+                        else {
+                            fovTop -= (0.125 * Math.PI);
+                            fovBottom -= (0.125 * Math.PI);
+                        }
+                        break;
+                    case KeyEvent.VK_A:
+                    case KeyEvent.VK_LEFT:
+                        if (isNormalMode) shiftX += 100;
+                        break;
+                    case KeyEvent.VK_S:
+                    case KeyEvent.VK_DOWN:
+                        if (isNormalMode) shiftY -= 100;
+                        else {
+                            fovTop += (0.125 * Math.PI);
+                            fovBottom += (0.125 * Math.PI);
+                        }
+                        break;
+                    case KeyEvent.VK_D:
+                    case KeyEvent.VK_RIGHT:
+                        if (isNormalMode) shiftX -= 100;
+                        break;
+                    case KeyEvent.VK_1:
+                        if (isNormalMode) zoom *= 2;
+                        else {
+                            fovTop += 0.25;
+                            fovBottom -= 0.25;
+                        }
+                        break;
+                    case KeyEvent.VK_2:
+                        if (isNormalMode) zoom /= 2;
+                        else {
+                            fovTop -= 0.25;
+                            fovBottom += 0.25;
+                        }
+                        break;
+                }
+
+                // Ensure fovTop and fovBottom are in the correct range
+                fovTop = standardizeAngle(fovTop);
+                fovBottom = standardizeAngle(fovBottom);
 
                 repaint();
             }
@@ -102,6 +141,7 @@ public class PlanetCanvas extends JPanel {
                 g.setColor(Color.WHITE);
             }
         } else {
+            // Set X and Y coordinates of viewPoint to followPlanet, or 0, 0 if that is not set
             double viewX, viewY;
 
             if (followPlanet == -1) {
@@ -116,20 +156,28 @@ public class PlanetCanvas extends JPanel {
                 // Skip planet we are currently following
                 if (followPlanet != -1 && planets.get(followPlanet).equals(planet)) continue;
 
-                // Normalize coordinates
+                // Normalize coordinates by getting coordinates relative to the viewpoint
                 double xRelative = planet.getPositionX() - viewX;
                 double yRelative = planet.getPositionY() - viewY;
 
-                // Ensure planet is visible
+                // Get relative angle from view planet
                 double viewAngle = Math.atan(yRelative / xRelative);
-                // TODO: Correct for tangent problems - kinda did it but now inefficient
-                // TODO: Add planet radius into equation
-                // if (viewAngle < fovBottom || viewAngle > fovTop) continue;
-                if ((Math.cos(fovBottom) * Math.cos(fovTop) - Math.sin(fovBottom) * Math.sin(fovTop)) * (Math.cos(fovBottom) * xRelative - Math.sin(fovTop) * yRelative) < 0) continue;
+                // Fix tangent flaws, ensure on same scale as fovTop and fovBottom
+                if (xRelative < 0) viewAngle += Math.PI;
+                viewAngle = standardizeAngle(viewAngle);
 
-                // Get position relative to bottom of screen
-                double angleToPixelConversion = getHeight() / (fovTop - fovBottom);
-                double yDraw = (viewAngle - fovBottom) * angleToPixelConversion;
+                // Skip planet if not within fovTop and fovBottom
+                if (fovTop > fovBottom) {
+                    if (viewAngle < fovBottom || viewAngle > fovTop) continue;
+                } else if (viewAngle < fovBottom && viewAngle > fovTop) continue;
+                // TODO: Add planet radius into equation
+
+                // Get position relative to bottom of screen, check for case that bottom is above top
+                double angleToPixelConversion = getHeight() / (fovTop > fovBottom ?  fovTop - fovBottom : fovTop - fovBottom + 2*Math.PI);
+
+                //Draw planet in correct spot, correcting for break in domain
+                double relativeAngle = viewAngle > fovBottom ? viewAngle - fovBottom : viewAngle - fovBottom + 2*Math.PI;
+                double yDraw = relativeAngle * angleToPixelConversion;
 
                 // Calculate the percent of the screen the planet takes up
                 double radius = planet.getRadius();
@@ -140,6 +188,7 @@ public class PlanetCanvas extends JPanel {
                 // Draw planet at calculated position
                 g.fillOval((int) (getWidth()/2 - newRadius), (int) (yDraw - newRadius), (int) (newRadius*2), (int) (newRadius*2));
 
+                // Draw line for better visibility
                 g.setColor(Color.RED);
                 g.fillRect(getWidth()/2 - 40, (int) yDraw - 1, 20, 2);
                 g.setColor(Color.WHITE);
@@ -154,12 +203,11 @@ public class PlanetCanvas extends JPanel {
         for(Planet planet : planets) {
             planet.update(planets);
         }
+        if (followPlanet != -1) {
+            shiftX = -planets.get(followPlanet).getPositionX();
+            shiftY = -planets.get(followPlanet).getPositionY();
+        };
         repaint();
-
-        if (followPlanet == -1) return;
-
-        shiftX = -planets.get(followPlanet).getPositionX();
-        shiftY = -planets.get(followPlanet).getPositionY();
     }
 
     /**
@@ -168,6 +216,9 @@ public class PlanetCanvas extends JPanel {
     public void setFollowPlanet() {
         followPlanet ++;
         if (followPlanet >= planets.size()) followPlanet = 0;
+        shiftX = -planets.get(followPlanet).getPositionX();
+        shiftY = -planets.get(followPlanet).getPositionY();
+        repaint();
     }
 
     /**
@@ -182,5 +233,18 @@ public class PlanetCanvas extends JPanel {
      */
     public void toggleViewMode() {
         isNormalMode = !isNormalMode;
+        repaint();
+    }
+
+    /**
+     * Takes in an angle in radians and returns an angle in the range -PI to PI
+     * @param angle the angle to standardize
+     * @return standardized angle
+     */
+    public double standardizeAngle(double angle) {
+        if (angle > Math.PI) angle -= 2*Math.PI;
+        if (angle <= -Math.PI) angle += 2*Math.PI;
+
+        return angle;
     }
 }
--- src/PlanetDraw.java
+++ src/PlanetDraw.java
@@ -26,26 +26,26 @@ public class PlanetDraw extends JFrame {
 //        canvas.addPlanet(new Planet(1, Math.sqrt(2.0/300), 0, 0, 300));
 
         // Binary star system with a planet orbiting each star and exoplanet and exoplanet moon
-//        canvas.addPlanet(new Planet(100000, Math.sqrt(1.0/800), 0, 0, 200));
-//        canvas.addPlanet(new Planet(100000, -Math.sqrt(1.0/800), 0, 0, -200));
-//        canvas.addPlanet(new Planet(1, Math.sqrt(1.0/25) + Math.sqrt(1.0/800), 0, 0, 225));
-//        canvas.addPlanet(new Planet(1, -Math.sqrt(1.0/25) - Math.sqrt(1.0/800), 0, 0, -225));
-//        canvas.addPlanet(new Planet(200, Math.sqrt(1.0/1000), 0, 0, 2000));
-//        canvas.addPlanet(new Planet(1, Math.sqrt(1.0/1000) + Math.sqrt(0.00066666), 0, 0, 2003));
+        canvas.addPlanet(new Planet(100000, Math.sqrt(1.0/800), 0, 0, 200));
+        canvas.addPlanet(new Planet(100000, -Math.sqrt(1.0/800), 0, 0, -200));
+        canvas.addPlanet(new Planet(1, Math.sqrt(1.0/25) + Math.sqrt(1.0/800), 0, 0, 225));
+        canvas.addPlanet(new Planet(1, -Math.sqrt(1.0/25) - Math.sqrt(1.0/800), 0, 0, -225));
+        canvas.addPlanet(new Planet(200, Math.sqrt(1.0/1000), 0, 0, 2000));
+        canvas.addPlanet(new Planet(1, Math.sqrt(1.0/1000) + Math.sqrt(0.00066666), 0, 0, 2003));
 
         // Earth Sun Moon representation, ratio of revolution time, ratio of orbit distance, mass ratios are all correct
         // Planet size is Not correct
-        canvas.addPlanet(new Planet(27068780, 0, 0, 0, 0, 18)); // The Sun
-        canvas.addPlanet(new Planet(81, Math.sqrt(270.0878/3902), 0, 0, 3902, 0.16)); // Earth
-        canvas.addPlanet(new Planet(1, Math.sqrt(270.0878/3902) + Math.sqrt(0.00081/10), 0, 0, 3912, 0.045)); // The Moon
-        canvas.addPlanet(new Planet(4.5, Math.sqrt(270.0878/1506), 0, 0, 1506, 0.063)); // Mercury
-        canvas.addPlanet(new Planet(66.3, Math.sqrt(270.0878/2810), 0, 0, 2810, 0.157)); // Venus
-        canvas.addPlanet(new Planet(8.7, Math.sqrt(270.0878/5931), 0, 0, 5931, 0.088)); // Mars
-        canvas.addPlanet(new Planet(25837, Math.sqrt(270.0878/20265), 0, 0, 20265, 1.82)); // Jupiter
-        canvas.addPlanet(new Planet(7739, Math.sqrt(270.0878/37122), 0, 0, 37122, 1.51)); // Saturn
-        canvas.addPlanet(new Planet(1179, Math.sqrt(270.0878/74687), 0, 0, 74687, 0.66)); // Uranus
-        canvas.addPlanet(new Planet(1390, Math.sqrt(270.0878/116987), 0, 0, 116987, 0.64)); // Neptune
-        canvas.addPlanet(new Planet(0.2, Math.sqrt(270.0878/153824), 0, 0, 153824, 0.031)); // Pluto
+//        canvas.addPlanet(new Planet(27068780, 0, 0, 0, 0, 18)); // The Sun
+//        canvas.addPlanet(new Planet(81, Math.sqrt(270.0878/3902), 0, 0, 3902, 0.16)); // Earth
+//        canvas.addPlanet(new Planet(1, Math.sqrt(270.0878/3902) + Math.sqrt(0.00081/10), 0, 0, 3912, 0.045)); // The Moon
+//        canvas.addPlanet(new Planet(4.5, Math.sqrt(270.0878/1506), 0, 0, 1506, 0.063)); // Mercury
+//        canvas.addPlanet(new Planet(66.3, Math.sqrt(270.0878/2810), 0, 0, 2810, 0.157)); // Venus
+//        canvas.addPlanet(new Planet(8.7, Math.sqrt(270.0878/5931), 0, 0, 5931, 0.088)); // Mars
+//        canvas.addPlanet(new Planet(25837, Math.sqrt(270.0878/20265), 0, 0, 20265, 1.82)); // Jupiter
+//        canvas.addPlanet(new Planet(7739, Math.sqrt(270.0878/37122), 0, 0, 37122, 1.51)); // Saturn
+//        canvas.addPlanet(new Planet(1179, Math.sqrt(270.0878/74687), 0, 0, 74687, 0.66)); // Uranus
+//        canvas.addPlanet(new Planet(1390, Math.sqrt(270.0878/116987), 0, 0, 116987, 0.64)); // Neptune
+//        canvas.addPlanet(new Planet(0.2, Math.sqrt(270.0878/153824), 0, 0, 153824, 0.031)); // Pluto
     }
 
     public static void main(String[] args) {
